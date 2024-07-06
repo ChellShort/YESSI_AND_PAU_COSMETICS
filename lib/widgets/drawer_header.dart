63cf0cb89772ee75fee1ca779c4686b3ce975421
Ricardo <119143917+ChellShort@users.noreply.github.com>
2024-07-05 20:15:13 -0600

    Vista de pedido cuenta

--- lib/widgets/drawer_header.dart
+++ lib/widgets/drawer_header.dart
@@ -1,26 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:yessi_pau/Views/pedido_cuenta.dart';
 
 class DrawerHeaderCustom extends StatelessWidget {
   const DrawerHeaderCustom({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const DrawerHeader(
-        decoration: BoxDecoration(
+    return DrawerHeader(
+        decoration: const BoxDecoration(
           color: Color.fromARGB(255, 188, 140, 190),
         ),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            CircleAvatar(
-              radius: 40,
-              child: Icon(
-                Icons.person,
-                size: 40,
+            GestureDetector(
+              child: const CircleAvatar(
+                radius: 40,
+                child: Icon(
+                  Icons.person,
+                  size: 40,
+                ),
+                
               ),
+              onTap: () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => pedidoCuenta(),
+                  ),
+                )
+              
+              },
             ),
-            SizedBox(width: 20),
-            Text('Usuario'),
+            const SizedBox(width: 20),
+            const Text('Usuario'),
           ],
         ));
   }
